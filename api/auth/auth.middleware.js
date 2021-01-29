124cd6d09d37e867efcf4b7e40edd02c27274104
mxhs <maxwellstofman@gmail.com>
2021-01-29 14:02:42 -0800

    try catch added in error routes

--- api/auth/auth.middleware.js
+++ api/auth/auth.middleware.js
@@ -3,16 +3,21 @@ const Auth = require("./auth-model");
 module.exports = { validateRegister, checkKeys, validateLogin };
 
 async function validateRegister(req, res, next) {
-	const user = await Auth.findBy({ username: req.body.username });
+	//? Why is try catch block needed for tests to pass? It worked before
+	try {
+		const user = await Auth.findBy({ username: req.body.username });
 
-	if (checkKeys(req.body)) {
-		res.status(401).json({ errorMessage: "invalid keys on request object" });
-	} else if (!req.body.username || !req.body.password) {
-		res.status(401).json({ errorMessage: "username and password required" });
-	} else if (user) {
-		res.status(401).json({ errorMessage: "username taken" });
-	} else {
-		next();
+		if (checkKeys(req.body)) {
+			res.status(401).json({ errorMessage: "invalid keys on request object" });
+		} else if (!req.body.username || !req.body.password) {
+			res.status(401).json({ errorMessage: "username and password required" });
+		} else if (user) {
+			res.status(401).json({ errorMessage: "username taken" });
+		} else {
+			next();
+		}
+	} catch (err) {
+		next(err);
 	}
 }
 
@@ -28,13 +33,17 @@ function checkKeys(obj) {
 }
 
 async function validateLogin(req, res, next) {
-	const user = await Auth.findBy({ username: req.body.username });
+	try {
+		const user = await Auth.findBy({ username: req.body.username });
 
-	if (!req.body.username || !req.body.password) {
-		res.status(401).json({ errorMessage: "username and password required" });
-	} else if (!user) {
-		res.status(401).json({ errorMessage: "invalid credentials" });
-	} else {
-		next();
+		if (!req.body.username || !req.body.password) {
+			res.status(401).json({ errorMessage: "username and password required" });
+		} else if (!user) {
+			res.status(401).json({ errorMessage: "invalid credentials" });
+		} else {
+			next();
+		}
+	} catch (err) {
+		next(err);
 	}
 }
