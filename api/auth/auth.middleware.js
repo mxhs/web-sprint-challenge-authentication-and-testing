2eac75983f957e25eb9d869eb32d36fb639e1edb
mxhs <maxwellstofman@gmail.com>
2021-01-31 13:27:55 -0800

    All tests passing

--- api/auth/auth.middleware.js
+++ api/auth/auth.middleware.js
@@ -5,12 +5,12 @@ module.exports = { validateRegister, checkKeys, validateLogin };
 async function validateRegister(req, res, next) {
 	//? Why is try catch block needed for tests to pass? It worked before
 	try {
+		if (!req.body.username || !req.body.password) {
+			res.status(401).json({ errorMessage: "username and password required" });
+		}
 		const user = await Auth.findBy({ username: req.body.username });
-
 		if (checkKeys(req.body)) {
 			res.status(401).json({ errorMessage: "invalid keys on request object" });
-		} else if (!req.body.username || !req.body.password) {
-			res.status(401).json({ errorMessage: "username and password required" });
 		} else if (user) {
 			res.status(401).json({ errorMessage: "username taken" });
 		} else {
@@ -34,11 +34,13 @@ function checkKeys(obj) {
 
 async function validateLogin(req, res, next) {
 	try {
-		const user = await Auth.findBy({ username: req.body.username });
-
 		if (!req.body.username || !req.body.password) {
 			res.status(401).json({ errorMessage: "username and password required" });
-		} else if (!user) {
+		}
+
+		const user = await Auth.findBy({ username: req.body.username });
+
+		if (!user) {
 			res.status(401).json({ errorMessage: "invalid credentials" });
 		} else {
 			next();
